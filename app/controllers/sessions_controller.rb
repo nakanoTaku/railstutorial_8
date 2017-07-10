384d38395a4da35ebbf36920c503699ca0da3265
taku <kosmo.waizu0804@gmail.com>
2017-07-10 14:11:13 +0900

    8章完

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,19 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.autheticate(params[:session][:password])
-        log_in(user)
-        redirect_to user
-      else
-        flash.now[:danger] = 'Invalid email/password combination'
-        render 'new'
-      end
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
-  def footeer
-
-
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
